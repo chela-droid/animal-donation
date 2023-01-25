327078daaa89739adbded90ebb685dca6e15af32
kevin.mutua@student.moringaschool.com <kevin.mutua@student.moringaschool.com>
2023-01-25 11:14:30 +0300

    changed Petcard, added Adoptpet, then feched pets from the API

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import '../App.css';
+import Home from './Home';
+import AdoptPet from './AdoptPet';
+
+const API = "http://localhost:8001/pets"
+
+
+function App() {
+
+  const [pets, setPets]= useState([])
+
+  useEffect(()=>{
+    fetch (API)
+    .then(res => res.json())
+    .then(setPets)
+
+  }, [])
+  return (
+    <>
+    <Home/>
+    <AdoptPet pets={pets}/>
+    </>
+  );
+}
+
+export default App;
