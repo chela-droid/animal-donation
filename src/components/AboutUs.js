9590646328d72c40b1ade2ba1c63ed4a0d0f5f97
brian.maina@student.moringaschool.com <brianmaina2>
2023-01-26 03:12:39 +0300

    created a search bar functionality

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -2,15 +2,21 @@ import React from 'react';
 
 const AboutUs = () => {
   return (
-    <div className="about-us">
-      <h1 style={{color:'#fba633'}}>About Us</h1>
-      <img src={{'https://www.milofoundation.org/wp-content/uploads/2020/06/kb_milo_62-scaled-847x1024.jpg'}}></img>
-      <p style={{textAlign: 'center'}}>Our mission is to connect loving families with deserving dogs in need of a home. We believe every dog deserves a chance at a happy and healthy life, and we strive to make that a reality through our platform.
-      We work closely with local shelters and rescue organizations to ensure that the dogs featured on our site are well-cared for and have undergone any necessary medical treatment. Our team thoroughly screens each potential adopter to ensure that each dog is placed in a loving and responsible home.
-      We also provide resources and support for new adopters to help them adjust to life with their new furry family member.
-      Thank you for considering adoption and for supporting our mission to find forever homes for dogs in need. Together, we can make a difference in the lives of these deserving animals.</p>
-    </div>
-  );
+        <div className="about-us">
+            <h1 style={{ color: '#fba633' }}>About Us</h1>
+            <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+                <img src={('https://www.milofoundation.org/wp-content/uploads/2020/06/kb_milo_62-scaled-847x1024.jpg')}></img>
+
+                <p style={{ fontSize: '30px', background: '#2878e8', marginTop: '10%' }}>At our organization, we believe that every dog deserves a loving home. That's why we've dedicated ourselves to rescuing and rehoming dogs in need. We've seen firsthand the transformative power of love and care, and we're committed to making a difference in the lives of as many dogs as possible.
+
+                    Our work is made possible by the generosity of donors like you. Your donation will go directly towards the care and rehabilitation of our rescue dogs. From providing food and shelter, to medical treatment and training, your donation will make a real impact in the life of a dog in need.
+
+                    Thank you for considering a donation to our organization. Together, we can give dogs the second chance they deserve.
+                </p>
+            </div>
+        </div>
+
+    );
 };
 
 export default AboutUs;
--- src/components/AdoptPet.js
+++ src/components/AdoptPet.js
@@ -1,19 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import PetCard from "./PetCard";
+import "../App.css"
 
- function AdoptPet({pets,}){
-  
-    return(
+function AdoptPet({ pets, }) {
+   const [nameChange, setNameChange] = useState("All")
+
+   function handleChange(event) {
+      setNameChange(event.target.value)
+   }
+
+   const selectedPets = pets.filter((pet) => {
+      if (nameChange === "All") return true;
+
+      return pet.name === nameChange
+   })
+
+   return (
+      <>
+         <div id="adopt1">
+            <input id="adoptpet" placeholder="search by name" onChange={handleChange}></input>
+            <button id="adopt">Refresh</button>
+         </div>
+
+
          <div className="ui four column grid">
             <div className="row">
-                {pets.map((pet) => (
-                <PetCard key={pet.id} pet={pet} />
-                ))}
+               {selectedPets.map((pet) => (
+                  <PetCard key={pet.id} pet={pet} />
+               ))}
+
 
             </div>
 
          </div>
+      </>
+   )
+}
 
-    )
- }
- export default AdoptPet;
+export default AdoptPet;
